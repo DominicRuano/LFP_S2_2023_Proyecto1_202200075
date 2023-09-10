9bc262ec4deac1fd0e984c51e949ae63a3138f10
DominicRuano <Dojuparupe1975@gmail.com>
2023-09-10 12:15:59 -0600

    cambios a la ventana

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from tkinter import ttk
 WIDTH = 900  # Ancho
 HEIGHT = 600 # Alto
 
+
+color = 0
+
 root = tk.Tk()
 root.title("Analizador lexico - Editor de texto")
 root.resizable(False, False)
@@ -15,27 +18,31 @@ ancho_pantalla = (root.winfo_screenwidth() - WIDTH) // 2
 alto_pantalla = (root.winfo_screenheight() - HEIGHT - 50) // 2 # 50 es la altura de la barra de tareas
 root.geometry("{}x{}+{}+{}".format(WIDTH, HEIGHT,ancho_pantalla, alto_pantalla))
 
+# Colorea el fondo de la ventana
 label_fondo = tk.Label(root, background="#343541")
 label_fondo.place(x=0, y=0, width=900, height=37)
 
+# Crearcion de estilo para los combobox
 style= ttk.Style()
 style.theme_use('clam')
-style.configure("TCombobox", fieldbackground= "black", background= "white")
+style.configure("TCombobox", fieldbackground= "#3E447D", background= "#3E447D", foreground= "white", bordercolor= "#343541", 
+                arrowcolor= "white", borderwidth= 0.5, lightcolor= "white", darkcolor= "black")
 
+# Creacion del combobox
 combobx = ttk.Combobox(root)
 combobx.place(x=100, y=100)
-combobx["values"] = ["ARCHIVO", "Abrir", "Guardar", "Guardar como", "Salir"]
-combobx.current(0)
+combobx["values"] = ["ARCHIVO", "Abrir", "Guardar", "Guardar como", "Salir"] # Valores del combobox
+combobx.current(0) # Valor por defecto del combobox
 combobx.configure(foreground='#E0E0E0')
 combobx.place(x=10, y=7, width=100, height=23)
 
-button = tk.Button(root, text="Analizador", bg="black", fg="white")
+button = tk.Button(root, text="Analizador", bg="#333766", fg="white", borderwidth=0.5)
 button.place(x=140, y=7, width=100, height=23)
 
-button2 = tk.Button(root, text="Errores", bg= "black", fg="white")
+button2 = tk.Button(root, text="Errores", bg= "#333766", fg="white", borderwidth=0.5)
 button2.place(x=270, y=7, width=100, height=23, )
 
-button3 = tk.Button(root, text="Reporte", bg="black", fg="white")
+button3 = tk.Button(root, text="Reporte", bg="#333766", fg="white", borderwidth=0.5)
 button3.place(x=400, y=7, width=100, height=23)
 
 entry = tk.Text(root, bg="#343541", fg="white")
